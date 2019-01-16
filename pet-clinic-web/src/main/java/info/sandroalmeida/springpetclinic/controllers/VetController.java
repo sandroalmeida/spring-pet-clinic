a385576fe3974e04bc5543cea1a25f8c889a4803
sandroalmeida <sandrodotbuild@gmail.com>
2019-01-16 05:22:15 -0800

    Added Vets List and updated Controller. Closes #15

--- pet-clinic-web/src/main/java/info/sandroalmeida/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/info/sandroalmeida/springpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package info.sandroalmeida.springpetclinic.controllers;
 
+import info.sandroalmeida.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
