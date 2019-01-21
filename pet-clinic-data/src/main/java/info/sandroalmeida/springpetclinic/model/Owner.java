ce0768184a6d3d0073e052666f3cc16ce4b3b511
sandroalmeida <sandrodotbuild@gmail.com>
2019-01-20 17:24:46 -0800

    Added Visit and update Owner with Pet relationship. Closes #26

--- pet-clinic-data/src/main/java/info/sandroalmeida/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/info/sandroalmeida/springpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package info.sandroalmeida.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by sandro on 09/01/19
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
