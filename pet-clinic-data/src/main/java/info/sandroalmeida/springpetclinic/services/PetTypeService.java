63804c6bd34bb0cb7b18d04f65723a99a12bebc2
sandroalmeida <sandrodotbuild@gmail.com>
2019-01-21 16:14:21 -0800

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/info/sandroalmeida/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package info.sandroalmeida.springpetclinic.services;
+
+import info.sandroalmeida.springpetclinic.model.PetType;
+
+/**
+ * Created by sandro on 21/01/19
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
